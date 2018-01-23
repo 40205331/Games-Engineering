c5ea20fc55ef2e3a9e68704c61c5fded557f70d8
NAPIER-MAIL\40205331 <40205331@live.napier.ac.uk>
2018-01-23 21:28:46 +0000

    started space invaders

---
+++ practical_2/main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include "ship.h"
+#include "game.h"
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+
+//...
+std::vector<Ship *> ships;
+
+void Load() {
+	Invader* inv = new Invader(sf::IntRect(0, 0, 32, 32), { 100, 100 });
+	ships.push_back(inv);
+
+	for (int r = 0; r < invaders_rows; ++r) {
+		auto rect = IntRect(r * 32, r* 32, 32, 32);
+		for (int c = 0; c < invaders_columns; ++c) {
+			Vector2f position = {c*50+10, r*50+10};
+			auto inv = new Invader(rect, position);
+			ships.push_back(inv);
+		}
+	}
+}
+
+void Update(double dt) {
+	for (auto &s : ships) {
+		s->Update(dt);
+	};
+}
+
+void Render(RenderWindow &window ) {
+	for (const auto s : ships) {
+		window.draw(*s);
+	}
+}
